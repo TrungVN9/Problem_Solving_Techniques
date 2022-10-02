dac4766b6e37aa28270dbccce6bc7ad8b1093e6d
Trung N Vong <trungnvong956@g.ucla.edu>
2022-10-02 16:21:35 -0700

    three sum O(n^2) using two pointers and one loop

--- Two_Pointers/3sum.py
+++ Two_Pointers/3sum.py
@@ -21,27 +21,41 @@ Notice that the order of the output and the order of the triplets does not matte
     =========
     Thought Process 
     [-8, -5, -4, -1, 0, 1, 2, 8]
-    0. create a temp_list = []
-    1. result = nums[start] + nums[end] + nums[temp_pointer]
-    2. if result < 0, then start is still, temp_pointer += 1, end is still.
-    3. Then if result == 0, return [nums[start], nums[temp_pointer], nums[end]]
-    4. if result > 0, end = end - 1
-
+    0. create a temp_list = [] to store the list of list three sum.
+    1. has 1 loop to traverse the list of length - 2 ele
+    2. has two pointers: left and right
+        2.1 left = index + 1
+        2.2 right = len(nums) - 1
+        2.3 if threeSum result > 0:
+            + right -= 1
+        2.4 if threeSum result < 0:
+            + left += 1
+        2.5 otherwise
+            + append to the temp list
+            + left += 1
+            + Check the case where left index has duplicate num by using loop
 '''
-def three_sum(nums: list(int)):
+def three_sum(nums: list[int]):
     nums.sort()
     temp_list = []
-    start = 0
-    end = len(nums) - 1
-    temp_pointer = 1
-    while start < end:
-        result = nums[start] + nums[temp_pointer] + nums[end]
-        if result == 0:
-            temp_list.append([nums[start], nums[temp_pointer], nums[end]])
-        if result < 0:
-
-        if result > 0:
-            end -= 1
-        if temp_pointer == end:
-            temp_pointer = start + 1
-        temp_pointer += 1
+    for index, value in enumerate(nums):
+        left = index + 1
+        right = len(nums) - 1  
+        if value == nums[index - 1] and index > 0: #Skip the iteration if the next val is duplicated
+            continue
+        while left < right:
+            result = value + nums[left] + nums[right]
+            if result > 0:
+                right -= 1
+            elif result < 0:
+                left += 1
+            else:
+                temp_list.append([value, nums[left], nums[right]])
+                left += 1
+                while nums[left] == nums[left - 1] and left < right:
+                    left += 1
+    return temp_list
+    
+            
+nums = [-8, -5, -4, -1, 0, 1, 2, 8]
+print(three_sum(nums))
